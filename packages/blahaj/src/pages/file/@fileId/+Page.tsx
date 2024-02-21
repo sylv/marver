620fc036093a3f3892604c12a8d058c890b05a5c
Sylver <ryan@sylver.me>
2024-02-22 01:19:18 +0800

    feat: remove partially implemented tags

--- packages/blahaj/src/pages/file/@fileId/+Page.tsx
+++ packages/blahaj/src/pages/file/@fileId/+Page.tsx
@@ -1,4 +1,3 @@
-import clsx from 'clsx';
 import type { FC } from 'react';
 import { useQuery } from 'urql';
 import { FileDocument, FileType } from '../../../@generated/graphql';
@@ -6,33 +5,8 @@ import { ImageOverlay } from '../../../components/image-overlay';
 import { Loading } from '../../../components/loading';
 import { Player } from '../../../components/player/player';
 import { Card } from '../../../components/ui/card';
-import { formatNumber } from '../../../helpers/format-number';
 import type { PageProps } from '../../../renderer/types';
 import { useMediaStore } from './media.store';
-
-const TAG_COLOURS = [
-  'text-red-500',
-  'text-green-500',
-  'text-blue-500',
-  'text-yellow-500',
-  'text-purple-500',
-  'text-pink-500',
-  'text-indigo-500',
-  'text-red-400',
-];
-
-const TEMP_TAGS = [
-  'cute',
-  'animal',
-  'mammal',
-  'vertebrate',
-  'genre:comedy',
-  'genre:action',
-  'genre:adventure',
-  'genre:romance',
-  'genre:horror',
-  'artist:john_doe',
-];
 
 export const Page: FC<PageProps> = ({ routeParams }) => {
   const fileId = routeParams.fileId!;
@@ -51,20 +25,6 @@ export const Page: FC<PageProps> = ({ routeParams }) => {
   return (
     <div className="container mx-auto mt-10 space-y-2">
       <h1 className="text-xl font-semibold truncate">{data.file.name}</h1>
-      <div className="flex flex-wrap gap-1">
-        {TEMP_TAGS.map((tag, index) => {
-          const colour = TAG_COLOURS[index % TAG_COLOURS.length];
-          return (
-            <button
-              key={tag}
-              className="bg-zinc-900 text-xs border rounded-full px-3 py-1 hover:bg-zinc-800 transition"
-            >
-              <span className={clsx(colour, 'font-semibold')}>{tag}</span>
-              <span className="text-muted-foreground ml-1 text-xs">{formatNumber(10_000)}</span>
-            </button>
-          );
-        })}
-      </div>
       <div className="grid grid-cols-6 gap-2">
         <div className="col-span-5">
           <Card className="bg-zinc-900 overflow-hidden">
