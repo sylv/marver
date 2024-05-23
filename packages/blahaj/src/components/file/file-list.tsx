2350e8ca3779adcffb2d555b88dc42b35c8f78c0
Sylver <ryan@sylver.me>
2024-05-23 12:16:49 +0800

    feat: split file gallery into separate component

---
+++ packages/blahaj/src/components/file/file-list.tsx
@@ -0,0 +1,80 @@
+import { memo, useEffect, useRef, useState } from "react";
+import { graphql, unmask, type FragmentType } from "../../@generated";
+import { FilePreview } from "./file-preview";
+
+const Frag = graphql(`
+    fragment FileListProps on FileEdge {
+        node {
+            id
+            info {
+                height
+                width
+            }
+            ...FilePreviewProps
+        }
+    }
+`);
+
+export interface FileListProps {
+  targetWidth?: number;
+  rowHeight?: number;
+  files?: FragmentType<typeof Frag>[];
+}
+
+export const FileList = memo<FileListProps>(({ files: filesFrag, targetWidth = 250, rowHeight = 200 }) => {
+  const [unloadWithHeight, setUnloadWithHeight] = useState<number | null>(null);
+  const files = unmask(Frag, filesFrag);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (containerRef.current && !entries[0].isIntersecting && containerRef.current.clientHeight >= 80) {
+          // unload when off screen by a large margin.
+          // helps with performance, though introduces some flickering if you scroll fast.
+          // todo: try reduce flickering.
+          setUnloadWithHeight(containerRef.current.clientHeight);
+        } else {
+          setUnloadWithHeight(null);
+        }
+      },
+      {
+        // this is the margin that the observer will use to determine if the element is in the viewport.
+        // so 300% here means it has to be off screen by 300% of the screen height to be considered off screen.
+        // the 200% buffer here is to try prevent flickering when scrolling. it could be higher/lower, 300% seems fine for perf+ux balance.
+        rootMargin: "300% 0px 300% 0px",
+      },
+    );
+
+    observer.observe(containerRef.current);
+    return () => observer.disconnect();
+  }, [containerRef]);
+
+  return (
+    <div
+      className="flex flex-wrap gap-2"
+      ref={containerRef}
+      style={{
+        height: unloadWithHeight || undefined,
+      }}
+    >
+      {!unloadWithHeight &&
+        files?.map(({ node: file }) => {
+          const aspectRatio = file.info.width && file.info.height ? file.info.width / file.info.height : 1;
+
+          return (
+            <FilePreview
+              key={file.id}
+              file={file}
+              style={{
+                width: aspectRatio * targetWidth,
+                flexGrow: aspectRatio * targetWidth,
+                height: rowHeight,
+              }}
+            />
+          );
+        })}
+    </div>
+  );
+});
--- packages/blahaj/src/components/file/file-query-segment.tsx
+++ packages/blahaj/src/components/file/file-query-segment.tsx
@@ -1,10 +1,10 @@
 import clsx from "clsx";
-import { memo, useEffect, useRef, useState } from "react";
+import { memo, useEffect, useRef } from "react";
 import { useQuery } from "urql";
 import { graphql } from "../../@generated";
 import type { FilesQueryVariables } from "../../@generated/graphql";
 import { cn } from "../../helpers/cn";
-import { FilePreview } from "./file-preview";
+import { FileList } from "./file-list";
 
 interface FileQuerySegmentProps {
   variables: FilesQueryVariables;
@@ -23,25 +23,15 @@ const FilesQuery = graphql(`
         hasNextPage
       }
       edges {
-        node {
-          id
-          thumbnailUrl
-          info {
-            height
-            width
-          }
-          ...FilePreviewProps
-        }
+        ...FileListProps
       }
     }
   }
 `);
 
 export const FileQuerySegment = memo<FileQuerySegmentProps>(
-  ({ variables, isLastPage, isFirstPage, targetWidth = 250, rowHeight = 200, onLoadMore }) => {
-    const [unloadWithHeight, setUnloadWithHeight] = useState<number | null>(null);
+  ({ variables, isLastPage, isFirstPage, targetWidth, rowHeight, onLoadMore }) => {
     const loaderRef = useRef<HTMLDivElement>(null);
-    const containerRef = useRef<HTMLDivElement>(null);
     const [{ fetching, data }] = useQuery({
       query: FilesQuery,
       variables: variables,
@@ -62,57 +52,10 @@ export const FileQuerySegment = memo<FileQuerySegmentProps>(
       return () => observer.disconnect();
     }, [loaderRef, isLastPage, data, fetching, onLoadMore]);
 
-    useEffect(() => {
-      if (!containerRef.current) return;
-      const observer = new IntersectionObserver(
-        (entries) => {
-          if (containerRef.current && !entries[0].isIntersecting && containerRef.current.clientHeight >= 80) {
-            // unload when off screen by a large margin.
-            // helps with performance, though introduces some flickering if you scroll fast.
-            // todo: try reduce flickering.
-            setUnloadWithHeight(containerRef.current.clientHeight);
-          } else {
-            setUnloadWithHeight(null);
-          }
-        },
-        {
-          // this is the margin that the observer will use to determine if the element is in the viewport.
-          // so 300% here means it has to be off screen by 300% of the screen height to be considered off screen.
-          // the 200% buffer here is to try prevent flickering when scrolling. it could be higher/lower, 300% seems fine for perf+ux balance.
-          rootMargin: "300% 0px 300% 0px",
-        },
-      );
-
-      observer.observe(containerRef.current);
-      return () => observer.disconnect();
-    }, [containerRef]);
-
     return (
-      <div
-        ref={containerRef}
-        className={cn("relative mb-2", isLastPage && "mb-20")}
-        style={{
-          height: unloadWithHeight || undefined,
-        }}
-      >
+      <div className={cn("relative mb-2", isLastPage && "mb-20")}>
         <div className="flex flex-wrap gap-2">
-          {!unloadWithHeight &&
-            data?.files.edges.map(({ node: file }) => {
-              const aspectRatio =
-                file.info.width && file.info.height ? file.info.width / file.info.height : 1;
-
-              return (
-                <FilePreview
-                  key={file.id}
-                  file={file}
-                  style={{
-                    width: aspectRatio * targetWidth,
-                    flexGrow: aspectRatio * targetWidth,
-                    height: rowHeight,
-                  }}
-                />
-              );
-            })}
+          <FileList files={data?.files.edges} targetWidth={targetWidth} rowHeight={rowHeight} />
         </div>
         {isLastPage && (
           <div
