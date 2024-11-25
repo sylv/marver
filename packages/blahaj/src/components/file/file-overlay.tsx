d44449e57ee974679449dd361ed87671ce743592
Sylver <ryan@sylver.me>
2024-11-25 18:12:25 +0800

    feat: file overlay animations

--- packages/blahaj/src/components/file/file-overlay.tsx
+++ packages/blahaj/src/components/file/file-overlay.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { memo, useEffect, useRef } from "react";
 import { graphql, unmask, type FragmentOf } from "../../graphql";
 import { useOnClickOutside } from "../../hooks/useOnClickOutside";
@@ -41,6 +41,7 @@ export const FileOverlay = memo<FileOverlayProps>(({ file: fileFrag, offsetTop, 
   }, [containerRef, file.id]);
 
   return (
+    // This handles animating between rows and opening/closing overlay animations
     <motion.div
       ref={containerRef}
       initial={{ opacity: 0 }}
@@ -50,12 +51,22 @@ export const FileOverlay = memo<FileOverlayProps>(({ file: fileFrag, offsetTop, 
       style={{ top: offsetTop, height }}
       className="absolute left-0 right-0 will-change-[opacity] bg-black/90 backdrop-blur-lg p-5 flex items-center justify-center z-20 scroll-m-6"
     >
-      <div>
-        <div className="font-lg text-2xl font-semibold">{file.displayName}</div>
-        <motion.div className="bg-black rounded-lg">
-          <FileContent file={file} className="max-h-[30em] w-min" />
+      {/* This handles animating between files in the same row, when the overlay itself doesn't move */}
+      <AnimatePresence mode="wait">
+        <motion.div
+          className="flex flex-col items-center"
+          key={file.id}
+          initial={{ translateY: -10, opacity: 0 }}
+          animate={{ translateY: 0, opacity: 1 }}
+          exit={{ translateY: 20, opacity: 0 }}
+          transition={{ duration: 0.1 }}
+        >
+          <div className="font-lg text-2xl font-semibold">{file.displayName}</div>
+          <motion.div className="bg-black rounded-lg overflow-hidden w-fit">
+            <FileContent file={file} className="max-h-[30em]" />
+          </motion.div>
         </motion.div>
-      </div>
+      </AnimatePresence>
     </motion.div>
   );
 });
